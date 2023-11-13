25640e2ddbeaf709d8abc43ae3aa2242a4a3339b
Jkrlos2006 <108955365+Jkrlos2006@users.noreply.github.com>
2023-11-12 21:15:58 -0500

    CASO PROPUESTO IDL-3

---
+++ BackEndTareas2/BackEndTareas2/Controllers/TareaController.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading;
+using BackEndTareas2.Models.DB;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackEndTareas2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareaController : ControllerBase
+    {
+        DbtareaContext db = new DbtareaContext();
+
+        [HttpGet]
+        public IActionResult ListarTodo()
+        {
+            List<Tarea> tareas = db.Tareas.ToList();
+            return Ok(tareas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult ObtenerPorId(int id)
+        {
+            Tarea tarea = db.Tareas.Find(id);
+            return Ok(tarea);
+        }
+
+        [HttpPost]
+        public IActionResult Crear([FromBody] Tarea request)
+        {
+            db.Tareas.Add(request);
+            db.SaveChanges();
+            return Ok(request);
+        }
+
+
+        [HttpPut]
+        public IActionResult Actualizar([FromBody] Tarea request)
+        {
+            db.Tareas.Update(request);
+            db.SaveChanges();
+            return Ok(request);
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult EliminarPorId(int id)
+        {
+            Tarea tarea = db.Tareas.Find(id);
+            db.Tareas.Remove(tarea);
+            db.SaveChanges();
+            return Ok(tarea);
+        }
+    }
+}
+
---
+++ BackEndTareas2/BackEndTareas2/Models/DB/DbtareaContext.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEndTareas2.Models.DB;
+
+public partial class DbtareaContext : DbContext
+{
+    public DbtareaContext()
+    {
+    }
+
+    public DbtareaContext(DbContextOptions<DbtareaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Tarea> Tareas { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=laptop-dbgbami9;Initial Catalog=DBTarea;Integrated Security=True;\nTrusted_Connection=true;Trust Server Certificate=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tarea>(entity =>
+        {
+            entity.ToTable("Tarea");
+
+            entity.Property(e => e.Descripcion)
+                .HasMaxLength(250)
+                .IsUnicode(false);
+            entity.Property(e => e.Estado)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Titulo)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
